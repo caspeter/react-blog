9ce5f29035c547d249527230f78911101da8a5a4
Cassie Peterson <cas.j.peters@gmail.com>
2017-02-26 16:57:46 -0700

    create new page and route to it for new post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux'; //isn't needed after our refactoring of the export
+import { fetchPosts } from '../actions/index.js';
 
-export default () => {
-  return <div>List of blog posts</div>;
+class PostIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return(
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+//This code does the same thing the export connect line does with fetchPosts
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
+
+export default connect(null, { fetchPosts })(PostIndex);
