668bf960f3e9847f83d8c629ad090363a9cda230
Cassie Peterson <cas.j.peters@gmail.com>
2017-02-26 21:33:02 -0700

    Clicking on a post on the main page will now bring you to a page with that id in the url

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostIndex from './components/posts_index.js';
 import PostsNew from './components/posts_new.js';
+import PostsShow from './components/posts_show.js';
 
 export default (
-<Route path="/" component={App}>
-  <IndexRoute component={PostIndex} />
-  <Route path="posts/new" component={PostsNew} />
-</Route>
-);
+  <Route path="/" component={App}>
+    <IndexRoute component={PostIndex} />
+    <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
+  </Route>
+      );
