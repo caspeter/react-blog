3865779de1ec58c63eb70bc73bf4d9a1f8023d2d
Cassie Peterson <cas.j.peters@gmail.com>
2017-02-26 20:23:48 -0700

    Add form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,67 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost  } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <div>Create Form</div>
-      
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create a New Post</h3>
+
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title}/>
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories}/>
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content}/>
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+
+      </form>
+
     );
   }
 }
 
-export default PostsNew;
+function validate(values){
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a title';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter a category';
+  }
+  if (!values.content) {
+    errors.content = 'Enter content';
+  }
+
+  return errors;
+}
+
+//connect: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
+//reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps(the action)
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields:['title', 'categories', 'content'],
+  validate
+}, null, { createPost })(PostsNew);
